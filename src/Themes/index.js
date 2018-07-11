de4634ca4eee99ad0d66e988ea454c949fdba7dd
Andrey Starkov <im@andreystarkov.ru>
2018-07-11 22:06:35 +0500

    remove wtf

--- src/Themes/index.js
+++ src/Themes/index.js
@@ -2,7 +2,6 @@ import Images from './Images'
 import Colors from './Colors'
 import Gradients from './Gradients'
 import Metrics from './Metrics'
-import SvgIcons from './SvgIcons'
 import { media } from './Media'
 
 import { rem } from 'Utils'
@@ -11,7 +10,6 @@ export {
   Colors,
   Gradients,
   Metrics,
-  SvgIcons,
   rem,
   media
 }
